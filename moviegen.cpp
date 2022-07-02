3f319461a9611b546c40021b514b4d4d3f7aadb6
HUANG Wenchong <ebola@zju.edu.cn>
2022-07-02 10:39:01 +0800

    Update args

--- moviegen.cpp
+++ moviegen.cpp
@@ -1,12 +1,13 @@
 #include <stdlib.h>
 #include <cstdio>
+#include <cmath>
 
 int main(int argc, char* argv[])
 {
     //double x = -0.72624685;
     //double y = 0.240377;
     double d = 5.0;
-    int n = 250;
+    double n = 250;
     char syscmd[80];
 
     double x = std::atof(argv[1]);
@@ -15,15 +16,17 @@ int main(int argc, char* argv[])
 
     system("mkdir img");
 
-    for(int i = 0; i < 3000; i++)
+    for(int i = 0; i < 1500; i++)
     {
-        sprintf(syscmd, "./picgen img/pic%d.png %.8lf %.8lf %.15lf %d", i, x, y, d, n);
+        sprintf(syscmd, "./picgen img/pic%d.png %.8lf %.8lf %.15lf %d", i, x, y, d, (int)round(n));
         if(i%4==branch)  //branch的用途见此处
         {
             printf("%s\n", syscmd);
             system(syscmd);
         }
         d *= 0.99;
+        d *= 0.99;
+        n *= 1.002313162;
         n *= 1.002313162;
     }
 
