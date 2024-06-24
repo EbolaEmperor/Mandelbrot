3d0169993b939b1998976ec5407c0881574cafcc
wenchong <wchung.huang@gmail.com>
2024-06-24 12:30:06 +0800

    random load balance

--- src/picgen.cpp
+++ src/picgen.cpp
@@ -6,6 +6,8 @@
 #include <cmath>
 #include <string>
 #include <mpi.h>
+#include <vector>
+#include <algorithm>
 
 void mandelbrotGen(IterFunc fun, 
                    const double maxConvergeRadio, 
@@ -23,34 +25,40 @@ void mandelbrotGen(IterFunc fun,
     axis myAxis(winWidth, winHeight, center, diam);  //根据中心和直径定义一个像素点坐标-轴坐标转换器
     png myPng(myAxis.getWinHeight(), myAxis.getWinWidth());  //初始化一个png
 
-    int nProcs, procID;
+    int nProcs, procID, cnt = 0;
     MPI_Comm_size(MPI_COMM_WORLD, &nProcs);
     MPI_Comm_rank(MPI_COMM_WORLD, &procID);
-    int L = procID * (myAxis.getWinHeight() / nProcs);
-    int R = (procID == nProcs - 1) ? 
-            myAxis.getWinHeight() :
-            (procID + 1) * (myAxis.getWinHeight() / nProcs); 
-    for(int i = L; i < R; i++)
-        for(int j = 0; j < myAxis.getWinWidth(); j++){
-            Cordinate cord = myAxis.pix2cord((Pix){i,j});  //计算像素点(i,j)对应的轴坐标
-            FunIter man(fun, maxConvergeRadio, 0.0, 0.0, cord.x, cord.y, N);   //给定迭代常数定义一个Mandelbrot迭代器
-            while(!man.unconverged()){ //进行迭代
-                man.iterate();
-                if(man.reachedMaxTimes())
-                    break;
-            }
-            if(man.unconverged())  //对不收敛区域进行着色
-                myPng.setpix(i, j, colorFunc1(man.getIterTimes()));
-            else
-                myPng.setpix(i, j, BLACK);
+    srand(19260817);
+    std::vector<Pix> allPix(myAxis.getWinHeight() * myAxis.getWinWidth());
+    for(int i = 0; i < myAxis.getWinHeight(); ++i)
+        for(int j = 0; j < myAxis.getWinWidth(); ++j)
+            allPix[cnt++] = (Pix){i, j};
+    std::random_shuffle(allPix.begin(), allPix.end());
+    int L = procID * (allPix.size() / nProcs);
+    int R = (procID == nProcs - 1) ? allPix.size() : (procID + 1) * (allPix.size() / nProcs);
+    for(int i = L; i < R && i < allPix.size(); i++){
+        auto& pix = allPix[i];
+        Cordinate cord = myAxis.pix2cord(pix);
+        FunIter man(fun, maxConvergeRadio, 0.0, 0.0, cord.x, cord.y, N);   //给定迭代常数定义一个Mandelbrot迭代器
+        while(!man.unconverged()){ //进行迭代
+            man.iterate();
+            if(man.reachedMaxTimes())
+                break;
         }
+        if(man.unconverged())  //对不收敛区域进行着色
+            myPng.setpix(pix.x, pix.y, colorFunc1(man.getIterTimes()));
+        else
+            myPng.setpix(pix.x, pix.y, BLACK);
+        if(i % 100 == 0)
+            std::cout << "[Proc " << procID << "]: " << (double)(i-L+1)/(R-L+1)*100 << "%" << std::endl;
+    }
     
     myPng.sync();
     if(procID == 0)
         myPng.output(fname);
 }
 
-/*
+
 void juliaGen(IterFunc fun, 
               const double maxConvergeRadio, 
               const char *Fname, 
@@ -86,4 +94,3 @@ void juliaGen(IterFunc fun,
 
     myPng.output(fname);
 }
-*/
